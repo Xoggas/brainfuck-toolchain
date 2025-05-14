81efb76fa6e0e0b38b88f2241b56c4526aa0a3d3
Xoggas <sashakor16@gmail.com>
2025-05-14 20:33:59 +0300

    Fixed static compilation path

--- BrainfuckToCSharp/Program.cs
+++ BrainfuckToCSharp/Program.cs
@@ -2,8 +2,6 @@
 
 public static class Program
 {
-    private static string s_basePath = @"C:\Users\raspisanie\Documents\GitHub\brainfuck-to-asm\BrainfuckToAsm";
-    
     public static void Main(string[] args)
     {
         if (args.Length != 1)
@@ -22,10 +20,10 @@ public static class Program
 
         var parser = new Parser(fileContent);
 
-        var operations = parser.Parse().ToArray();
+        var operations = parser.Parse();
 
         var transpiler = new Transpiler(operations);
         
-        File.WriteAllText(Path.Combine(s_basePath, "Output.cs"), transpiler.Transpile());
+        File.WriteAllText("Output.cs", transpiler.Transpile());
     }
 }
